61d0462ec1b3386ccf0098de1877546d174fc502
cherrybells <florescherrynicole@gmail.com>
2026-02-28 20:03:29 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let move_speed = 2;
+let move_speed = 1.5;
 let gravity = 0.35;
 let initialGravity = 0.05; // slow start
 let gravityIncreaseRate = 0.01; // slower ramp for first posts
@@ -37,7 +37,7 @@ function startGame() {
 
 function jump() {
     if (game_state === 'Play') {
-        bird_dy = -3.5; // reduced jump for easier control
+        bird_dy = -4.0; // reduced jump for easier control
     }
 }
 
